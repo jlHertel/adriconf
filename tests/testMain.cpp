78f9e01234fb02b9ccbdb57139ec0dc8143d4aba
Jean Hertel <jean.hertel@hotmail.com>
2018-03-24 10:30:17 -0300

    Remove unused GTest main function. Improves #7

--- tests/testMain.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "gtest/gtest.h"
-
-GTEST_API_ int main(int argc, char **argv) {
-    std::cout << "Start running tests" << std::endl;
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
