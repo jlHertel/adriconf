a7dfd800dff59ac3be4711524adcbc06c3128856
Jean Hertel <jean.hertel@hotmail.com>
2018-03-24 12:29:31 -0300

    Tests: Add more tests for Configuration resolver.

--- tests/ConfigurationResolverTest.cpp
+++ tests/ConfigurationResolverTest.cpp
@@ -43,3 +43,108 @@ TEST_F(UpdatePrimeApplicationsTest, CorrectGPU) {
     EXPECT_EQ("pci-testing-10", app->getDevicePCIId());
     EXPECT_EQ("i965", app->getPrimeDriverName());
 }
+
+
+class AddMissingDriverOptionsTest : public ::testing::Test {
+public:
+    Application_ptr app;
+
+    AddMissingDriverOptionsTest() {
+        app = std::make_shared<Application>();
+
+    }
+};
+
+/* Checks if the missing options have been added */
+TEST_F(AddMissingDriverOptionsTest, missingOptionCount) {
+    std::map<Glib::ustring, Glib::ustring> driverOptions;
+    driverOptions["bo_reuse"] = "1";
+    driverOptions["mesa_no_errors"] = "0";
+
+    ConfigurationResolver::addMissingDriverOptions(app, driverOptions);
+
+    EXPECT_EQ(2, app->getOptions().size());
+}
+
+/* Checks if the already existing options maintain their values */
+TEST_F(AddMissingDriverOptionsTest, unchangedOptionValue) {
+    ApplicationOption_ptr appOpt = std::make_shared<ApplicationOption>();
+    appOpt->setName("bo_reuse");
+    appOpt->setValue("0");
+
+    app->addOption(appOpt);
+
+
+    std::map<Glib::ustring, Glib::ustring> driverOptions;
+    driverOptions["bo_reuse"] = "1";
+    driverOptions["mesa_no_errors"] = "0";
+
+    ConfigurationResolver::addMissingDriverOptions(app, driverOptions);
+
+    std::map<Glib::ustring, Glib::ustring> appOptions = app->getOptionsAsMap();
+
+    EXPECT_EQ("0", appOptions["bo_reuse"]);
+    EXPECT_EQ("0", appOptions["mesa_no_errors"]);
+}
+
+
+class RemoveInvalidDriversTest : public ::testing::Test {
+public:
+    std::list<DriverConfiguration> availableDrivers;
+
+    RemoveInvalidDriversTest() {
+        DriverConfiguration conf1, conf2;
+        conf1.setDriverName("i965");
+        conf1.setScreen(0);
+
+        conf2.setDriverName("r600g");
+        conf2.setScreen(1);
+
+        availableDrivers.emplace_back(conf1);
+        availableDrivers.emplace_back(conf2);
+    }
+};
+
+TEST_F(RemoveInvalidDriversTest, incorrectScreenNumber) {
+    std::list<Device_ptr> userDefinedDevices;
+    Device_ptr d = std::make_shared<Device>();
+    d->setScreen(0);
+    d->setDriver("r600g");
+
+    userDefinedDevices.emplace_back(d);
+
+    ConfigurationResolver::removeInvalidDrivers(availableDrivers, userDefinedDevices);
+
+    EXPECT_EQ(0, userDefinedDevices.size());
+}
+
+TEST_F(RemoveInvalidDriversTest, incorrectDriverName) {
+    std::list<Device_ptr> userDefinedDevices;
+    Device_ptr d = std::make_shared<Device>();
+    d->setScreen(1);
+    d->setDriver("i965");
+
+    userDefinedDevices.emplace_back(d);
+
+    ConfigurationResolver::removeInvalidDrivers(availableDrivers, userDefinedDevices);
+
+    EXPECT_EQ(0, userDefinedDevices.size());
+}
+
+TEST_F(RemoveInvalidDriversTest, correctScreenAndDriver) {
+    std::list<Device_ptr> userDefinedDevices;
+    Device_ptr d = std::make_shared<Device>();
+    d->setScreen(0);
+    d->setDriver("i965");
+
+    Device_ptr d2 = std::make_shared<Device>();
+    d2->setScreen(1);
+    d2->setDriver("r600g");
+
+    userDefinedDevices.emplace_back(d);
+    userDefinedDevices.emplace_back(d2);
+
+    ConfigurationResolver::removeInvalidDrivers(availableDrivers, userDefinedDevices);
+
+    EXPECT_EQ(2, userDefinedDevices.size());
+}
