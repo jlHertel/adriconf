d148b196ea565d13d4cd0e63a58ab0c5d161bf2e
Jean Hertel <jean.hertel@hotmail.com>
2019-04-21 12:24:44 +0200

    Add support for new system-wide configurations location
    Signed-off-by: Jean Hertel <jean.hertel@hotmail.com>

--- adriconf/ConfigurationLoader.cpp
+++ adriconf/ConfigurationLoader.cpp
@@ -1,6 +1,9 @@
 #include "ConfigurationLoader.h"
+#include "ConfigurationResolver.h"
+#include <boost/filesystem.hpp>
 
 #include <fstream>
+#include <algorithm>
 
 Glib::ustring ConfigurationLoader::readSystemWideXML() {
     Glib::ustring container;
@@ -46,9 +49,35 @@ std::map<Glib::ustring, GPUInfo_ptr> ConfigurationLoader::loadAvailableGPUs(cons
 std::list<Device_ptr> ConfigurationLoader::loadSystemWideConfiguration() {
     std::list<Device_ptr> systemWideDevices;
 
+    std::vector<Glib::ustring> configurationPaths;
+    for (const auto &file : boost::filesystem::directory_iterator(this->getSystemWideConfigurationPath())) {
+        if (!boost::filesystem::is_directory(file)) {
+            configurationPaths.emplace_back(file.path().string());
+        }
+    }
+
+    std::sort(configurationPaths.begin(), configurationPaths.end());
+
+    for (auto &filename : configurationPaths) {
+        std::cout << "Found config under: " << filename << std::endl;
+        Glib::ustring container;
+        std::ostringstream buffer;
+        std::ifstream input(filename);
+        if (!input.good()) {
+            continue;
+        }
+
+        buffer << input.rdbuf();
+        container = buffer.str();
+        std::list<Device_ptr> justLoadedDevices = Parser::parseDevices(container);
+
+        ConfigurationResolver::mergeConfigurationOnTopOf(systemWideDevices, justLoadedDevices);
+    }
+
     Glib::ustring systemWideXML = this->readSystemWideXML();
     if (!systemWideXML.empty()) {
-        systemWideDevices = Parser::parseDevices(systemWideXML);
+        std::list<Device_ptr> justLoadedDevices = Parser::parseDevices(systemWideXML);
+        ConfigurationResolver::mergeConfigurationOnTopOf(systemWideDevices, justLoadedDevices);
     }
 
     /* In case no configuration is available system-wide we generate an empty one */
@@ -78,4 +107,14 @@ Glib::ustring ConfigurationLoader::getOldSystemWideConfigurationPath() {
     }
 
     return path;
+}
+
+boost::filesystem::path ConfigurationLoader::getSystemWideConfigurationPath() {
+    std::ifstream flatpakInfo("/.flatpak-info");
+
+    if(flatpakInfo.good()) {
+        return boost::filesystem::path("/var/run/host/usr/share/drirc.d/");
+    }
+
+    return boost::filesystem::path("/usr/share/drirc.d/");
 }
